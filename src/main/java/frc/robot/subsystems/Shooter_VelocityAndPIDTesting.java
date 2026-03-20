339fc271424dc1f8bacfc25775f16133a42c9bb6
Javip3710 <javip3710@gmail.com>
2026-03-20 11:00:54 -0700

    Update Shooter_VelocityAndPIDTesting.java

--- src/main/java/frc/robot/subsystems/Shooter_VelocityAndPIDTesting.java
+++ src/main/java/frc/robot/subsystems/Shooter_VelocityAndPIDTesting.java
@@ -5,7 +5,10 @@
 package frc.robot.subsystems;
 
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -16,60 +19,62 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants;
+
 public class Shooter_VelocityAndPIDTesting extends SubsystemBase {
 
   TalonFX motor1, motor2;
 
-
-  ShuffleboardTab tab = Shuffleboard.getTab("ShooterPIDVelocity");
+  public ShuffleboardTab tab = Shuffleboard.getTab("ShooterPIDVelocity");
   GenericEntry shooterPower = tab.add("Target Velocity: ", 0).getEntry();
-  GenericEntry vpid = tab.add("kV: ", 0).getEntry();
-  GenericEntry ppid = tab.add("kP: ", 0).getEntry();
-  GenericEntry ipid = tab.add("kI: ", 0).getEntry();
-  GenericEntry dpid = tab.add("kD: ", 0).getEntry();
-
-
 
   double Targetspeed = shooterPower.getDouble(0);
 
-  final VoltageOut request1 = new VoltageOut(0);
+  final MotionMagicVelocityVoltage request2 = new MotionMagicVelocityVoltage(0).withEnableFOC(true);
+
+
+  final NeutralOut requestN = new NeutralOut();
+
 
 
   /** Creates a new Shooter_VelocityAndPIDTesting. */
   public Shooter_VelocityAndPIDTesting() {
 
-  motor1 = new TalonFX(0);
-  motor2 = new TalonFX(0);
-  
-
+  // motor1 = new TalonFX(30);
+  motor1 = new TalonFX(30, "CANivore");
+  motor2 = new TalonFX(31, "CANivore");
 
   var TalonFXConfigs = new TalonFXConfiguration();
   var Slot0Configs = TalonFXConfigs.Slot0;
 
-  Slot0Configs.kV = vpid.getDouble(0);
-  Slot0Configs.kP = ppid.getDouble(0);
-  Slot0Configs.kI = ipid.getDouble(0);
-  Slot0Configs.kD = dpid.getDouble(0);
+  Slot0Configs.kV = 0.0075;
+  Slot0Configs.kP = 0.01;
+  Slot0Configs.kI = 0;
+  Slot0Configs.kD = 0;
 
-  motor1.getConfigurator().apply(Slot0Configs);
-  motor2.getConfigurator().apply(Slot0Configs);
+
+
+  var motionMagicConfigs = TalonFXConfigs.MotionMagic;
+    motionMagicConfigs.MotionMagicAcceleration = 400; // Target acceleration of 400 rps/s (0.25 seconds to max)
+    motionMagicConfigs.MotionMagicJerk = 4000; // Target jerk of 4000 rps/s/s (0.1 seconds)
+
+
+  motor1.getConfigurator().apply(TalonFXConfigs);
+  motor2.getConfigurator().apply(TalonFXConfigs);
   motor1.setNeutralMode(NeutralModeValue.Coast);
   motor2.setNeutralMode(NeutralModeValue.Coast);
   }
 
-  public void Start(){
-    motor1.setControl(request1.withOutput(Targetspeed));
-    motor2.setControl(request1.withOutput(-Targetspeed));
+  public void Start(){ //in RPS(revolutions per second)
+    // motor1.setControl(request1.withVelocity(Targetspeed));
+    // motor2.setControl(request1.withVelocity(-Targetspeed));
+    motor1.setControl(request2.withVelocity(Targetspeed));
+    motor2.setControl(request2.withVelocity(-Targetspeed));
   }
 
-  public void Shoot(double speed){ //in RPS
-    motor1.setControl(request1.withOutput(speed));
-    motor2.setControl(request1.withOutput(-speed));
-  }
-
-  public void Stop(){
-    motor1.setControl(new VelocityVoltage(0));
-    motor2.setControl(new VelocityVoltage(0));
+  public void setNeutral(){ 
+    motor1.setControl(requestN);
+    motor2.setControl(requestN);
   }
 
   @Override
